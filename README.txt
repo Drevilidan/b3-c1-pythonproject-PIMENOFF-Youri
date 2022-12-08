1c6539a0af861b7b11bedf367f7b8353cec6229d
Youri Pimenoff <monsieurpimenoff@gmail.com>
2022-12-08 12:29:26 +0100

    https://github.com/Drevilidan/b3-c1-pythonproject-PIMENOFF

---
+++ README.txt
@@ -0,0 +1,13 @@
+Besoin :
+
+- Bouton parcourir pour que l'utilisateur puisse chercher le csv à filtrer
+- Un bouton destination pour envoyer le fichier filtré à la destination voulu
+
+ Fonction :
+
+- Mettre un système d'exception de ligne
+- Choisir les colonnes à trier
+- ou lire le csv au préalable pour afficher les colonnes et les exceptions de ligne à trier
+
+- Supprimer toutes les lignes avec au moins une cellule vide
+- Faire des additions, soustractions, multiplications, divisions de colonne
---
+++ test button parcourir.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3 -i
+# Script boutton parcourir
+
+try:
+# Python 3 (version que tout le monde devrait utiliser !)
+    from tkinter import Button
+    from tkinter import Entry
+    from tkinter import StringVar
+    from tkinter import Tk
+    from tkinter.filedialog import askopenfilename
+
+except ImportError:
+
+    # Python 2
+    from Tkinter import Button
+    from Tkinter import Entry
+    from Tkinter import StringVar
+    from Tkinter import Tk
+    from tkFileDialog import askopenfilename
+
+FILETYPES = [ ("text files", "*.txt") ]
+
+
+root = Tk()
+
+filename = StringVar(root)
+
+entry = Entry(root, textvariable=filename)
+entry.pack()
+
+# button = Button( root,
+#                  text='Open',
+#                  lambda: filename.set(askopenfilename(filetypes=FILETYPES)) )
+# button.pack()
+
+# Ou alors :
+
+def set_filename():
+    filename.set(askopenfilename(filetypes=FILETYPES))
+
+button = Button(root, text='Parcourir', command=set_filename)
+button.pack()
+
+
+root.mainloop()
